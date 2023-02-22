60bb10a1072a6e91bbeb83276f80d106bc1c652c
David Perez <david.ezequiel.pere@gmail.com>
2023-02-22 13:37:14 -0300

    Added a couple of most common examples of delays on js thread

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ function delay(duration) {
 
 app.get('/', (req, res) => {
     res.send('Performance Example')
+    //JSON.stringify({}) => "{}"
+    //JSON.parse("{}") => {}
+    //[5,2,3,4].sort() (for very large arrays or very large objects it can block our code)
 })
 
 app.get('/timer', (req, res) => {
